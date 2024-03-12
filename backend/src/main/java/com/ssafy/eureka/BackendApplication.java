0ca783aec9092e36c931a22a9e2aedacee951b7d
yeongdae <oooo_0@naver.com>
2024-03-12 12:16:47 +0900

    Init: backend 프로젝트 생성
    - backend springboot 프로젝트 생성
    - build.gradle 설정
    - 프로젝트 폴더 구조 설정
    
    Chage-Id: Irehg4s15s6h5edr1fxb523415feasdharwbs
    Change-Id: I3bf4b97261fb722916df871ea14a2ae822aaa1b8

---
+++ backend/src/main/java/com/ssafy/eureka/BackendApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76624c86ca28d671014c8c8cc2aca870de02fc56044074b98516788d4bde56e6
+size 311
---
+++ backend/src/test/java/com/ssafy/eureka/BackendApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:560852ae6ef4007458f273090ac842609090329e24f663e61a0d3b09fca23a89
+size 209
