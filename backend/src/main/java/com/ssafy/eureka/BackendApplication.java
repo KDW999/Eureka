17d514dc6b47f0422eb54677b40c62e2e27381e4
yeongdae <oooo_0@naver.com>
2024-03-12 14:27:38 +0900

    Feat: application.yml 파일 설정
    - logging, swagger, spring
    
    Change-Id: I5e7b13198ff8729d5767b1e8be683b2bfa06ae33
    
    Former-commit-id: c8f956040debab2d78030cf719ef76fe4b85bc12

--- backend/src/main/java/com/ssafy/eureka/BackendApplication.java
+++ backend/src/main/java/com/ssafy/eureka/BackendApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:76624c86ca28d671014c8c8cc2aca870de02fc56044074b98516788d4bde56e6
-size 311
+package com.ssafy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
--- backend/src/test/java/com/ssafy/eureka/BackendApplicationTests.java
+++ backend/src/test/java/com/ssafy/eureka/BackendApplicationTests.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:560852ae6ef4007458f273090ac842609090329e24f663e61a0d3b09fca23a89
-size 209
+package com.ssafy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
