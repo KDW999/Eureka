9b883ea849d448b471632bebd323a222548d373f
kdw <hide4321@naver.com>
2024-03-13 12:38:32 +0900

    Init: 카드사 서버 프로젝트 생성
    Change-Id: Ia1d2ba5c609b4649a56c13399c98b39e1a7493b4
    
    Former-commit-id: 1de32b1e080f2236c52d64adc8586ad53d43f221

---
+++ card/src/main/java/com/ssafy/card/CardApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0bdd92ec803d3b36edfdebff711d8a38897f9d4607381921b44af320628beab
+size 315
---
+++ card/src/main/java/com/ssafy/card/common/TestController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b7a6eff011c9762b781631018e7ce643b12262d96db70fe257f7bac6856d1cc
+size 439
