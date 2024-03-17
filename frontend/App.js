6e2ee9685db9b39801bfc00533ef71c538d1ec8d
이승집 <edward626kr@naver.com>
2024-03-17 16:27:25 +0900

    Feat: 네비게이션바 및 페이지구성
    Change-Id: I4ac73bffaf38bbf040295ac138a00452badff15d
    
    Former-commit-id: b234d4a19904e6ba6ce68cc73f70af4cdd41fc0b

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f5c4da6157b8f8f741f6e3fba1d072358a518a44fa5838aa8f72fedb5fe507b
-size 454
+oid sha256:0b277a24b39ced450835c4c7beee20bb9323b2052e42e32bfada4eec03272764
+size 394
