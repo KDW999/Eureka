65dca6f1e1210a9937366d3370d129a8275fb66f
이승집 <edward626kr@naver.com>
2024-03-17 23:22:44 +0900

    Feat: 홈 화면 구성요소 및 레이아웃
    Change-Id: I5ecf89e17d78cb73ded02a2fa2c4ed3fdebb2959
    
    Former-commit-id: 3f45ff80c63679ad528e1c5573cc9974ec14de70

--- frontend/src/pages/HomePage.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a7792994377e13c48f4d8d8fcafe9ac094daa944dd9b952805a8f5722aff1fa6
-size 325
---
+++ frontend/src/pages/HomePage/components/HomeOnlyPay.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b564fc084243e6d865215beb003f7f568b4ecbd37bd3b09de763ec54bbeae3c6
+size 489
