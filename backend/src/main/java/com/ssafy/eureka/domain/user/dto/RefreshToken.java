01c1e46003a835f311a3a5e3e4761af9549bf7b2
yeongdae <oooo_0@naver.com>
2024-03-18 16:58:17 +0900

    Feat: JWT Token & Redis
    - config
    - repository (crud)
    
    Change-Id: I4e229f16923c4054529917ae661c3ac564cd8788
    
    Former-commit-id: 300fb07acf24927b0b93d44f9e015d3d896bc2ff

--- backend/src/main/java/com/ssafy/eureka/domain/user/dto/RefreshToken.java
+++ backend/src/main/java/com/ssafy/eureka/domain/user/dto/RefreshToken.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeb4b1dd5f67bb119a635a6001c9494a42b0ed691651f361730588cd1e5eecaa
-size 376
+oid sha256:bd07b815374707b352fc464f9c6232055119cf0aa6f7974500d359063cd19a44
+size 403
