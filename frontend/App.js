3b765f80a9c72604d291f44ddf0f08355bb24add
최용훈 <cyh2607@gmail.com>
2024-03-18 15:41:07 +0900

    FEAT:loading Page
    Change-Id: Id7c2086a577f9a2294e7234a9a6268007d071891
    
    Former-commit-id: 7a30372ef7ff904c09ed628b5f9cbcdb2bc9a1ba

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b277a24b39ced450835c4c7beee20bb9323b2052e42e32bfada4eec03272764
-size 394
+oid sha256:85fa3d39e8aff3174279ef21f0982e2d6f58d9328e04fc813484627eef05b135
+size 772
