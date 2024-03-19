e303172d30815f2aff1cb588c526ebea6b21b381
yeongdae <oooo_0@naver.com>
2024-03-19 17:24:24 +0900

    Feat: S3 설정
    - 의존성 추가, config
    - service는 만들지 않음
    
    Change-Id: Ice6d51d8ab2cfc4a5af2be95777c8ff2ef8ea28c
    
    Former-commit-id: 8b7b35327b665d9fb5f97fe41207ed72b3db1be8

---
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/UserCardEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3bd863beb789c849b02832562f07ec4cb34eb65018e075a7d564f42099ad1cff
+size 73
---
+++ backend/src/main/java/com/ssafy/eureka/domain/card/util/CarDataUtil.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55a101fa53f3ad8dc61138cccadfac6ae52195a8d50a739865a0f1c95f58d918
+size 71
