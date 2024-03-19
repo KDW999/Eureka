6f775906771980d05e7eaf59885b12865ee77a1f
최용훈 <cyh2607@gmail.com>
2024-03-19 17:26:15 +0900

    Feat: sign up form
    Change-Id: Ic139439e273fd1e8b4dc4ef81b62702b2eab31d1
    
    Former-commit-id: 07d4a869bb1aa61eccd16265ab3fb1062ca16502

--- card/src/main/java/com/ssafy/card/config/SecurityConfig.java
+++ card/src/main/java/com/ssafy/card/config/SecurityConfig.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c5b7e6818683df24abf3d458fa97f7154b17f676a3bf0b7a321ec7c5c5a5231a
-size 64
+package com.ssafy.card.config;
+
+public class SecurityConfig {
+}
