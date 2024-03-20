f0479fe61889d28383987dbf3d069edad3b9bf45
oksehun <sehun9803@naver.com>
2024-03-20 14:15:05 +0900

    feat: create MyAkaList
    Change-Id: I38406793c13ce3d43840b394cbfdd20e43b10684
    
    Former-commit-id: 82670f98ab1292be1f55a03ba4d89978128b1f94

--- backend/src/main/java/com/ssafy/eureka/common/exception/CustomException.java
+++ backend/src/main/java/com/ssafy/eureka/common/exception/CustomException.java
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ead518f42cdd4a8042dd6bb0376b3c26afa31ea41744e59a872aa3a622f755e2
-size 455
+package com.ssafy.eureka.common.exception;
+
+import com.ssafy.eureka.common.response.ResponseCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+    private final int code;
+    private final String message;
+
+    public CustomException(ResponseCode responseHeader) {
+        super(responseHeader.getMessage());
+        this.code = responseHeader.getCode();
+        this.message = responseHeader.getMessage();
+    }
+}
