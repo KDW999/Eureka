28995cb8bb3289a3c1751797d5bcbc248a9b05da
yeongdae <oooo_0@naver.com>
2024-03-22 10:09:50 +0900

    Feat: 유저 카드 관련 Cleint와 Requset, Response Dto 생성
    - Pay, History는 Class만
    
    Change-Id: I4719610a02cebcf3c0c524bac67f6d377babbfea
    
    Former-commit-id: 9aad21fc6863fc340984f1091dce830e2de02f23

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/request/SearchUserCardRequest.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/request/SearchUserCardRequest.java
@@ -1,2 +1,3 @@
-package com.ssafy.eureka.domain.card.dto.request;public class SearchUserCardRequest {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:59ac966c1c8af50d81b2101a2b4b04dbe34ee79c3108ccf3e6a28fa189f3488b
+size 186
--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/UserCardListResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/UserCardListResponse.java
@@ -1,2 +1,3 @@
-package com.ssafy.eureka.domain.card.dto.response;public class UserCardListResponse {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:32381634b46f5e93bda42112e5f76a23cc5746bf1dfafda93436c18e45d1c2e2
+size 383
