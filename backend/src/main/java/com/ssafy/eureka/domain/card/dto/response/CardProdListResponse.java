ca626e2af4ae138ae80f229bc30095ad8183b709
kdw <hide4321@naver.com>
2024-03-22 09:43:30 +0900

    Fix: 카드 상품 카드사 데이터 수정
    Change-Id: Ie99084c88b2b8702fa083e200627065cc717f113
    
    Former-commit-id: 365ba71e81bfaf0d57aa29a247e9a505204e77d7

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardProdListResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardProdListResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ded198895801834585d179a091db8a8943d8f9acbedf203d3e13710c3933cca
-size 455
+oid sha256:4c2fd6df54aaeb2e93d486d1924dba9a4c6ed9d515030cfd57b6b30e7ca01970
+size 488
