409ad7e83acca03b905eed8ee91f19f14f9b10a1
yeongdae <oooo_0@naver.com>
2024-03-24 16:36:42 +0900

    Feat: QR코드 추가 ( 간단한 결제 정보 설정해놨음 )
    Change-Id: Ifd7afe37147cb401ca8a3d6a5b339b575588614b
    
    Former-commit-id: ede0ba2d3f480cd4ad9a1ff0237206e15748d2ac

--- frontend-web/src/pages/MainPage.jsx
+++ frontend-web/src/pages/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:579dd03fe287129ca74ed8569add7d3efbb341af1627777da25c2885f55a358c
-size 235
+oid sha256:6cb5d5e3da164026166de233ade175ff491b3903fea00544eada3129f5ba0567
+size 318
