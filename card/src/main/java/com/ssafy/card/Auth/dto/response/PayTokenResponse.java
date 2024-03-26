fd0dcd3f32403ebd4ea07dcb878a4868c68b9e2b
김해인 <khi762@naver.com>
2024-03-26 09:50:39 +0900

    Docs: 결제내역 마이데이터 더미
    Change-Id: I2706011fa0200b870732924b102a2015b6e43c88
    
    Former-commit-id: 325ddfcc1254efa490820fe720a60a73b0ab6721

--- card/src/main/java/com/ssafy/card/Auth/dto/response/PayTokenResponse.java
+++ card/src/main/java/com/ssafy/card/Auth/dto/response/PayTokenResponse.java
@@ -1,18 +1,3 @@
-package com.ssafy.card.Auth.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class PayTokenResponse {
-    private int cardId;
-    private String cardIdentifier;
-    private String grantType;
-    private String accessToken;
-    private String refreshToken;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc956e4eb53b7e022fb5b034bad13325de9d40c2c8b42a7b54f1b8eaa8d2de84
+size 398
