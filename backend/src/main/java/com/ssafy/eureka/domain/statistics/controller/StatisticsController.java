cc57d87e219178b8b75298f2eb9e01a4c46548eb
김해인 <khi762@naver.com>
2024-03-27 07:47:31 +0900

    Feat: 소비 통계 엔티티 생성
    Change-Id: I92956b4df691d7c0706be37af687a3681590153d
    
    Former-commit-id: fe055027a99b4fbdb975350c37c4f00cdc9d5e79

---
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/controller/StatisticsController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:990705a2e6c9d770aaabc9f50a75bd978cb337fe6f3d0392fe4a8e3ff982acb2
+size 260
---
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionStaticRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dee2282766b607b8ca21595d013974b8b39e5186d577c5a9d718de082488c70d
+size 298
