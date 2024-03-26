ac1f88bf8602309f734ffc6b8811c3891e0bfd54
이승집 <edward626kr@gmail.com>
2024-03-26 14:59:44 +0900

    Fix: App.js 주석 삭제
    Change-Id: Ia7afdb281abf928c5971383d8ffd39616231b274
    
    Former-commit-id: 2b5c41843a0125b9658bd9c41e783239f9f22286

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dab0aba6d4cf12d7375baa02fae2dd7bdf47627fc9b746534dd0b00be852dd19
-size 909
+oid sha256:7960c8298e67b0edfbc6f88e593dca7d2ff04f9ba2b9c46611fae182a22ca326
+size 444
