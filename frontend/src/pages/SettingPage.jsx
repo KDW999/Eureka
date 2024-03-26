18b7a5972a7fec0db4152c16c58d9f1112cb5cd8
kdw <hide4321@naver.com>
2024-03-27 00:45:28 +0900

    Fix: PayHistory Logic Fix
    Change-Id: Ie6ef6d93194905b06cff4d069acb56598e64a2a9
    
    Former-commit-id: d7546a51b89d421200e11bbe2d809af354968223

--- frontend/src/pages/SettingPage.jsx
+++ frontend/src/pages/SettingPage.jsx
@@ -1,5 +1,3 @@
-function SettingPage () {
-
-}
-
-export default SettingPage
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d303f06c7e0685aafcdf03a6783b08195df882d5e604bfa3302cd756e09de58
+size 56
