5169079a4f8cc9c0af41ff5f70d05d324685b526
kdw <hide4321@naver.com>
2024-03-29 02:49:56 +0900

    Feat: 페이 결제 내역
    Change-Id: I63cb944ee88d4e0d8288e43009533a3ac9a43b91
    
    Former-commit-id: 0e06f536236841e003253127e3f1fa8b833f3f7d

--- card/src/main/java/com/ssafy/card/Pay/service/implementation/PayServiceImpl.java
+++ card/src/main/java/com/ssafy/card/Pay/service/implementation/PayServiceImpl.java
@@ -1,6 +1,3 @@
-package com.ssafy.card.Pay.service.implementation;
-
-import com.ssafy.card.Pay.service.PayService;
-
-public class PayServiceImpl implements PayService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9298c0a87813b1b354eef4f52d28d77be4f196b34222df2be2f6489c11916484
+size 153
