774cdd6be086405a73bdcdaa15a4082261f2c17d
yeongdae <oooo_0@naver.com>
2024-03-28 23:46:09 +0900

    Feat: 카드사 결제 결제 승인 API 구현
    Change-Id: Ia8aa3b85ec013241958f9f8e2020d2fa4a1e8e14
    
    Former-commit-id: 1262cfc4edd4027590c2c976a88c4cca4122837c

--- card/src/main/java/com/ssafy/card/Card/dto/response/ApprovePayResponse.java
+++ card/src/main/java/com/ssafy/card/Card/dto/response/ApprovePayResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c522d4e994fcdbdf2a7701630cba18f377e3f1f20ab933482bbea5b5babfdfa
-size 77
+oid sha256:df86de3c00ede43babaef0cd11ef81cdeeb4daa058a98f08627f0851f015c8c6
+size 1557
