b052de0b759d75cfed3b81a26d5bf68a4e3b5bff
kdw <hide4321@naver.com>
2024-03-28 21:47:36 +0900

    Fix: 카드 정보 이미지, 방향 추가
    Change-Id: Ia1d738af5d8436e25d8effa1537d71a56a742778
    
    Former-commit-id: cb98f99d28b259446f6482552940fca2efb1e1be

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardInfoResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardInfoResponse.java
@@ -1,20 +1,3 @@
-package com.ssafy.eureka.domain.card.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CardInfoResponse {
-
-    int userCardId;
-    int cardId;
-    String cardName;
-    int cardType;
-    int previousPerformance;
-    boolean isPaymentEnabled;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fdbe9bdb35ee32f0b135ced58737f9b31d080f0893658e3caf41f38cb98c404b
+size 427
