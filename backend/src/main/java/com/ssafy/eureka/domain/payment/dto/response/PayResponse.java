5e2df2c17e812309aaff48193c36bd044ccbcdb6
김해인 <khi762@naver.com>
2024-03-28 15:22:24 +0900

    Fix: 빌드오류 수정 - 에셋 이름 변경
    Change-Id: Ie6d9cd3ce94139466003ba2f6295865006baeda0
    
    Former-commit-id: 5ac81183d58749b5c5abe5c5b43f4a239224f762

--- backend/src/main/java/com/ssafy/eureka/domain/payment/dto/response/PayResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/payment/dto/response/PayResponse.java
@@ -1,8 +1,3 @@
-package com.ssafy.eureka.domain.payment.dto.response;
-
-import lombok.Getter;
-
-@Getter
-public class PayResponse {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ac62c3bdf3f957901d14b5576ee62569781b3a8e644c5a7bc5d685943199e06
+size 116
