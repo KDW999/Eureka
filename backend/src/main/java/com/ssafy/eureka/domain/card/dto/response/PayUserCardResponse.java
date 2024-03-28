d0d7eae842b4b85ceb1ca0c869b2aa3ade494a71
oksehun <sehun9803@naver.com>
2024-03-28 17:52:31 +0900

    Feat: product-page modal and MainNotice
    Change-Id: I5c3da30b6c3e45641af6e36c414d40576f1347c1
    
    Former-commit-id: 1fedfa7344a5b8a2e1c110cf9cad3e389b294eb8

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/PayUserCardResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/PayUserCardResponse.java
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1f1c78cf6234305e134b73969d6218c6dce8dd341eb34d306213fd9b28a5e52d
-size 473
+package com.ssafy.eureka.domain.card.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PayUserCardResponse {
+
+    int userCardId;
+    int userId;
+    int cardId;
+    String cardName;
+    int previousPerformance;
+    // ++ 현재까지 사용 금액
+    String firstCardNumber;
+    String lastCardNumber;
+    String imagePath;
+}
