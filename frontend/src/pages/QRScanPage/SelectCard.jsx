0431aa3c002b7d3a6cff41ee7326c3a42ed32301
yeongdae <oooo_0@naver.com>
2024-03-29 11:58:26 +0900

    Feat: 결제 내역 조회 통꼐 추가
    Change-Id: Id8fbaa380f7b87e13928e8ba5d1f6a36e8966bcb
    
    Former-commit-id: ae9a59127891f2ff741b07f1c3ce5473caf32d3e

--- frontend/src/pages/QRScanPage/SelectCard.jsx
+++ frontend/src/pages/QRScanPage/SelectCard.jsx
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7bde01275ea13f7057108504b15889cb469cbcc826d8906d31eb156e13587de8
-size 56
+function SelectCard () {
+  
+}
+
+export default SelectCard
