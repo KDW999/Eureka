7ba3c3f0a5a56a7a17b92bade7bbb522f24d8321
yeongdae <oooo_0@naver.com>
2024-03-29 14:26:57 +0900

    Feat: 결제 API 구현 (테스트 완) 및 db 수정
    Change-Id: Ie8a2d76b17fac3f6410f986d19719bf354f00314
    
    Former-commit-id: 086e1084f9c386770e721fb4ecef6dec76e79e74

--- card/src/main/java/com/ssafy/card/Card/dto/request/ApprovePayRequest.java
+++ card/src/main/java/com/ssafy/card/Card/dto/request/ApprovePayRequest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92ff4bd8e3b6200cd4e41c2bbe92a8a345897f58351d24cd81fae2dfecd2888a
-size 531
+oid sha256:c31dc024aa45eb678c3df7764e1906009113dea4917a333f9dd4881a264961e6
+size 471
