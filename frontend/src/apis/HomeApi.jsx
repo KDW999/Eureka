b3a7b47dea4425f0a8e3fa35c0883a7ff06b724c
kdw <hide4321@naver.com>
2024-03-30 14:36:54 +0900

    Fix: 보유 카드 통계 저장 에러
    Change-Id: I8835843c79aeec30a95d9a294325749d8b897823
    
    Former-commit-id: 7a8e109a73acc3c753929de97640d8d4983c4010

--- frontend/src/apis/HomeApi.jsx
+++ frontend/src/apis/HomeApi.jsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2cab62b4a0439714e5f6b2274f5f10e19818e7a953c7a05a30988f0b6a03334a
-size 243
+import API from "./Api";
+
+async function getHomeInfo(token, yyyymm, success, fail) {
+  try {
+    const response = await API(token).get(`/static/${yyyymm}`)
+    success(response);
+  } catch (error) {
+    fail(error);
+  }
+}
+
+export {getHomeInfo}
