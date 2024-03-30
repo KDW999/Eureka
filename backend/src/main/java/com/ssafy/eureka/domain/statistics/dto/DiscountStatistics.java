abf9dffb661721d22739b0345c686b489ce7a8c3
이승집 <edward626kr@naver.com>
2024-03-30 15:33:00 +0900

    Feat: 우리 페이 결제 금액
    Change-Id: Id7e550afbb25038c21b035a237fe48bcf3a7f160
    
    Former-commit-id: d4199ad7cdee16502b068c50d689449ac949da27

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/DiscountStatistics.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/DiscountStatistics.java
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7d2e56c27b0989366dfec0f8aff9bbcbd309e6b5c344cd721ef4467ee02eebcf
-size 340
+package com.ssafy.eureka.domain.statistics.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiscountStatistics {
+    private int categoryId;
+    private String categoryName;
+    private Long discount;
+}
--- backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/TotalStatistics.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/TotalStatistics.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ebcda81a18458900fb8f93484cd0f85e40ecb0686c32912d044f882d24c0b73a
-size 258
+package com.ssafy.eureka.domain.statistics.dto;
+
+import lombok.*;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TotalStatistics {
+    private BigInteger totalConsumption;
+    private Long totalDiscount;
+}
