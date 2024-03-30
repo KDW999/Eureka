2a29a44333d360224365b3551c9c770a277b56b8
yeongdae <oooo_0@naver.com>
2024-03-30 21:10:55 +0900

    Feat: 결제 시 통계에 반영되도록 로직 추가
    - 나중에 따로 뺄 예정 / 지금 엄청 느림
    
    Change-Id: Ibe8c92dc5740a33dbbc9545b361d806542f2c29b
    
    Former-commit-id: a2c05370e07c2b4975f147dddedbfd3ffc6e05d7

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/DiscountSmallStaticRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/DiscountSmallStaticRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b1de5c453b4bfbe5400ae1d40d83be395014d4be7aac0e16f9f56ad2861788c
-size 303
+oid sha256:7712bad230cd127922ff3f0993eaf0b6303d02e0d543ae3d1cada712580e55b8
+size 470
