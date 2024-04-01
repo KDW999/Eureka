20b24a4f2c2a4a290d20d74f8fc071393dea9d00
이승집 <edward626kr@gmail.com>
2024-04-01 15:40:56 +0900

    Feat: 카드 등록시 모달창& 결제 완료
    Change-Id: If48c159154796e91808bd10c03964ac1b4b03b84
    
    Former-commit-id: ad9b5e49dff48027ff7fbb8a57e658b77b4b987f

--- backend/src/main/java/com/ssafy/eureka/domain/pay/util/PayUtil.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/util/PayUtil.java
@@ -1,21 +1,3 @@
-package com.ssafy.eureka.domain.pay.util;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class PayUtil {
-
-
-
-    // 결제 추천 함수
-    public int calculateDiscount() {
-
-
-
-
-        return 0;
-    }
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:206492cd1da9afba01d2d0d31313f63edd2b8cd6ceb0191ac85cca092cd18aa0
+size 290
