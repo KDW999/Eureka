0677bb66e5b3d12900aa12ed76c469bb8363c993
김해인 <khi762@naver.com>
2024-04-01 21:35:46 +0900

    Fix: 가장 최근 데이터만 조회하는 로직 추가
    Change-Id: I78569b21d0171e78d0c78870956393c25d3f3d98
    
    Former-commit-id: 8538e1a5dcad653f3f2c52b705e31489f0843bc3

--- backend/src/main/java/com/ssafy/eureka/BackendApplication.java
+++ backend/src/main/java/com/ssafy/eureka/BackendApplication.java
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f0db972578eb037c2f74b4372d40248a72e6aea969177294c1ca5aade4e3b601
-size 479
+package com.ssafy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
