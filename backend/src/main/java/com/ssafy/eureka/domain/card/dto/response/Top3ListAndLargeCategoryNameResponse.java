c89e2453f4efb2e37555612bb315f16365e291ae
옥세훈 <sehun9803@gmail.com>
2024-04-03 07:45:53 +0900

    Feat: productpage
    Change-Id: I66db3cb17ae231f94fff2d5f8e4291d6d99e0766
    
    Former-commit-id: 34b448251d98a0ba04e133f51079c8a9d781202f

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/Top3ListAndLargeCategoryNameResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/Top3ListAndLargeCategoryNameResponse.java
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4796091ec4e9fc12b3f14dc02abfd3f5280686358ef407dc3b640bbaa49043e6
-size 366
+package com.ssafy.eureka.domain.card.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Top3ListAndLargeCategoryNameResponse {
+
+    String largeCategoryName;
+    List<CardRecommendTop3List> list;
+}
