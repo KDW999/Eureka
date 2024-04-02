e0aebd357d0028ee4a2fecf370e71a29292c5bea
kdw <hide4321@naver.com>
2024-04-02 17:57:23 +0900

    Feat: 추천 카드 상위 카테고리 3개에 사용 시 혜택 비용 추가
    Change-Id: Iab58090d1929b21d7f7b74676d0dd11372703a71
    
    Former-commit-id: 111e41790b9fed7c700109c6d6cbd82ed7ed95af

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardRecommendTop3.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardRecommendTop3.java
@@ -1,21 +1,3 @@
-package com.ssafy.eureka.domain.card.dto.response;
-
-import com.ssafy.eureka.domain.card.dto.CardBenefitDetailEntity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.List;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CardRecommendTop3 {
-
-    String cardName;
-    String imagePath;
-    int imgAttr;
-    List<CardRecommendTop3List> list;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:53c356d6a4a8aacb2f74510dc75ac8d562975b92f58972c0328b7e62aa8a38d6
+size 462
